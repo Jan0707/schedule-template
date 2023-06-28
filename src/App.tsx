fac44c103e507d9140073e10dec7c85c39fb58fd
Jan0707 <jan@jangregor.me>
2023-06-28 15:09:27 +0200

    Added basic schedule grid

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,61 @@ import './style.css';
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div className="container">
+      <div className="schedule-header">
+        <div>
+          <h1>Media: Conception & Production</h1>
+        </div>
+        <div>
+          <h2>Montag, 26. Juni 2023</h2>
+        </div>
+      </div>
+      <div className="schedule-container">
+        <div className="schedule-column column-timing">
+          <div>KW 99</div>
+          <div>08:15 - 09:00</div>
+          <div>09:00 - 09:45</div>
+          <div>10:00 - 10:45</div>
+          <div>10:45 - 11:30</div>
+          <div>11:45 - 12:30</div>
+          <div>12:30 - 13:15</div>
+          <div>13:15 - 14:15</div>
+          <div>14:15 - 15:00</div>
+          <div>15:00 - 15:45</div>
+          <div>16:00 - 16:45</div>
+          <div>16:45 - 17:30</div>
+          <div>17:45 - 18:30</div>
+          <div>18:30 - 19:15</div>
+        </div>
+        <div className="schedule-columns columns-semesters">
+          <div className="column-semester">
+            <div>1. Semester</div>
+            <div className="semester-events"></div>
+          </div>
+          <div className="column-semester">
+            <div>2. Semester</div>
+            <div className="semester-events"></div>
+          </div>
+          <div className="column-semester">
+            <div>3. Semester</div>
+            <div className="semester-events"></div>
+          </div>
+        </div>
+        <div className="schedule-column column-details">
+          <div className="detail-entry">
+            <p>1. Semester / 08:15 – 13:15</p>
+            <p>
+              Media Technology Basics
+              <br />
+              • F3-0-20
+              <br />
+              • TS, StS
+              <br />
+              Gruppe 1; bitte Aufteilung und Dokumente in StudIP beachten ...
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
